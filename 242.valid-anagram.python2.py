03102a5230f783f293583760c70fad59bdb0e3fd
GorenLagen <serezha.belikov.05@mail.ru>
2025-01-02 22:13:35 +0300

    242

--- 242.valid-anagram.python2.py
+++ 242.valid-anagram.python2.py
@@ -6,33 +6,54 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        count = 0
-        sum_s, sum_t = 0, 0
-        for i in range(len(s)):
-            sum_s += ord(s[i])
-        for i in range(len(t)):
-            sum_t += ord(t[i])
-        if sum_t == sum_s and (len(s) == len(t)):
-            for i in range(len(s)):
-                if (s[i] in t) and (t[i] in s):
-                    count += 1
-                    if count == len(s):
-                        return True
 
+        if len(s) == len(t):
+
+            dict_t = {}
+            dict_s = {}
+            count = 0
+            len_ = len(s)
+
+            for i in range(len_):
+                if s[i] not in dict_s:
+                    dict_s[s[i]] = 1
+                elif s[i] in dict_s:
+                    value = dict_s[s[i]]
+                    dict_s[s[i]] = value + 1
+
+            for i in range(len_):
+                if t[i] not in dict_t:
+                    dict_t[t[i]] = 1
+                elif t[i] in dict_t:
+                    value = dict_t[t[i]]
+                    dict_t[t[i]] = value + 1
+
+            for j in range(len_):
+                if s[j] in dict_t:
+                    if dict_s[s[j]] == dict_t[s[j]]:
+                        count += 1
+                        if count == len_:
+                            return True
+                    else:
+                        return False
                 else:
                     return False
+
         else:
             return False
 
 
 sol = Solution()
-s = "ggii"
-t = "eekk"
+# s = "ggii"
+# t = "eekk"
 
 s = "anagtam"
 t = "nbgbram"
 
-s = "acacbac"
-t = "bbbbbac"
+# s = "acacbac"
+# t = "bbbbbac"
+
+# s = "abbba"
+# t = "ababb"
 print(sol.isAnagram(s=s, t=t))
 # @leet end
