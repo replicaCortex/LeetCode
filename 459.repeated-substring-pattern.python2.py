006c6c2269c9858463c7dd65e9dbf7df13c04427
GorenLagen <serezha.belikov.05@mail.ru>
2025-01-03 13:33:22 +0300

    03.01.2025
    fix 459

--- 459.repeated-substring-pattern.python2.py
+++ 459.repeated-substring-pattern.python2.py
@@ -3,28 +3,12 @@
 
 class Solution(object):
     def repeatedSubstringPattern(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-
-        tmp = []
-        for i in range(len(s) - 1):
-            tmp.append(s[0 : i + 1])
-
-        for k in range(len(tmp)):
-            for j in range(0, len(s), len(tmp[k])):
-                if (s[j : len(tmp[k]) + j]) == tmp[k]:
-                    # print(s[j : len(tmp[k]) + j])
-                    pass
-                else:
-                    tmp[k] = 0
-                    break
-
-        for p in range(len(tmp)):
-            if tmp[p] != 0:
-                return True
-
+        n = len(s)
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                substring = s[:i]
+                if substring * (n // i) == s:
+                    return True
         return False
 
 
@@ -35,7 +19,7 @@ class Solution(object):
 # s = "aba"
 #
 # s = "abcabcabcabc"
-#
-#
+# #
+# #
 # print(sol.repeatedSubstringPattern(s))
 # @leet end
