3d991d61020f68a9a52a632d3af6a4b83c9cea5d
replicaCortex <replicaCortex@gmail.com>
2025-02-02 12:05:30 +0300

    2025.02.02

---
+++ 141.linked-list-cycle.python2.py
@@ -0,0 +1,29 @@
+# @leet start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head == None:
+            return False
+
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            if slow == fast:
+                return True
+
+            slow = slow.next
+            fast = fast.next.next
+        return False
+
+
+# @leet end
