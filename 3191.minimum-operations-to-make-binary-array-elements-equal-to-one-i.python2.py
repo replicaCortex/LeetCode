dd2b0d321c4f1d2fce0404cde18f97237031ae43
replicaCortex <replicaCortex@gmail.com>
2025-03-20 10:53:49 +0300

    2025.03.19

--- 3191.minimum-operations-to-make-binary-array-elements-equal-to-one-i.python2.py
+++ 3191.minimum-operations-to-make-binary-array-elements-equal-to-one-i.python2.py
@@ -5,5 +5,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+
+        ans = 0
+        for start, end in zip(range(len(nums)), range(3, len(nums) + 1)):
+            if nums[start] != 1:
+                nums_window = nums[start:end]
+                if 0 in nums_window:
+                    ans += 1
+                    for current_num in range(start, end):
+                        if nums[current_num] != 1:
+                            nums[current_num] = 1
+                        else:
+                            nums[current_num] = 0
+
+        return ans if 0 not in nums else -1
+
+
 # @leet end
+sol = Solution()
+# nums = [0, 1, 1, 1, 0, 0]
+
+nums = [0, 1, 1, 1]
+print(sol.minOperations(nums))
